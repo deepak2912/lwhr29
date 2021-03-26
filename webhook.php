8aeaa43a8fc475e4291d5fc040cedda1968dab44
deepak2912 <81016886+deepak2912@users.noreply.github.com>
2021-03-26 12:46:10 +0530

    Add files via upload

---
+++ webhook.php
@@ -0,0 +1,14 @@
+<?php
+
+$challenge = $_REQUEST['hub_challenge'];
+$verify_token = $_REQUEST['hub_verify_token'];
+
+if ($verify_token === 'abc123') {
+  echo $challenge;
+}
+
+$input = json_decode(file_get_contents('php://input'), true);
+error_log(print_r($input, true));
+
+//https://tranquil-savannah-5375.herokuapp.com/webhook.php
+?>
